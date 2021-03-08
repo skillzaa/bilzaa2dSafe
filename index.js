e9fb713865a48cdc3ff2f087c391ceb75cada3bf
skillzaa <skillzaa.com@gmail.com>
2021-03-08 10:58:10 +0500

    safety

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ c.basicData.x=300;
 c.basicData.y=300;
 //bilzaa2d.animations.addMove()
 a.addAnimation(bilzaa2d.animations.addMove());
+a.addAnimation(bilzaa2d.animations.addMove());
+a.addAnimation(bilzaa2d.animations.addMove());
+
 b.addAnimation(bilzaa2d.animations.addMove());
 c.addAnimation(bilzaa2d.animations.addMove());
 
