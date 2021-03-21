99d0b3c88cfd6574bcf0ec86814d53b918f747c1
skillzaa <skillzaa.com@gmail.com>
2021-03-22 02:51:35 +0500

    improved the base naimation code and confusion between 4 input arrays into animation is also cleared

--- typescript/premtives/Text.ts
+++ typescript/premtives/Text.ts
@@ -3,11 +3,12 @@ import BasePrimtive from "./BasePrimtive.js";
 export default class Text extends BasePrimtive {
   constructor() {      
     super("text");
+    this.attributes.add({name: "title", value: "Text" });
   }
 
 draw() {
     if(this.attributes.getProperty("drawBorder") === true){
-    this.drawBorder();
+   // this.drawBorder();
     }
     this.metal.drawText(this.attributes.getProperty("title"),this.attributes.getProperty("x"),this.attributes.getProperty("y"));
 }
